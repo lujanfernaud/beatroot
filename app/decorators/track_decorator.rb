50c440a5584b48de27f23c0ad226e6ae2341b835
lujanfernaud <lujanfernaud@gmail.com>
2018-10-20 23:45:43 +0100

    Add TrackDecorator

---
+++ app/decorators/track_decorator.rb
@@ -0,0 +1,92 @@
+# Decorates track's json.
+class TrackDecorator
+  attr_reader :track
+
+  def initialize(track)
+    @track = track
+  end
+
+  def isrc
+    track["isrc"]
+  end
+
+  def reference_title?
+    title && subtitle
+  end
+
+  def title
+    track["title"]
+  end
+
+  def subtitle
+    track["subtitle"]
+  end
+
+  def duration?
+    track["duration"]
+  end
+
+  def duration_formatted
+    duration = track["duration"]
+
+    Time.at(duration).gmtime.strftime("PT%HH%MM%SS")
+  end
+
+  def artist_name
+    return unless artist
+
+    artist["name"]
+  end
+
+  def artist
+    track["artist"]
+  end
+
+  def direct_contributors
+    return unless contributors
+
+    contributors.select { |item| item["direct"] == true }
+  end
+
+  def contributors
+    track["contributors"]
+  end
+
+  def indirect_contributors
+    return unless contributors
+
+    contributors.select { |item| item["direct"] == false }
+  end
+
+  def record_label_name
+    record_labels = track["record_labels"]
+
+    return unless record_labels
+
+    if record_labels.first.present?
+      record_labels.first["name"]
+    end
+  end
+
+  def pline
+    record_labels = track["record_labels"]
+
+    return unless record_labels
+
+    if record_labels.first.present?
+      record_labels.first["p_line"]
+    end
+  end
+
+  def tags
+    track["tags"]
+  end
+
+  def parental_warning
+    explicit != nil
+  end
+
+  def explicit
+    track["explicit"]
+  end
+end
---
+++ spec/decorators/track_decorator_spec.rb
@@ -0,0 +1,153 @@
+require 'rails_helper'
+
+RSpec.describe TrackDecorator do
+  describe "when a json track is received" do
+    it "decorates 'isrc'" do
+      json = { "isrc" => "GBG3H1000203" }
+
+      track_decorated = TrackDecorator.new(json)
+
+      expect(track_decorated.isrc).to eq(json["isrc"])
+    end
+
+    it "decorates 'title'" do
+      json = { "title" => "Sweet Lorraine" }
+
+      track_decorated = TrackDecorator.new(json)
+
+      expect(track_decorated.title).to eq(json["title"])
+    end
+
+    it "decorates 'subtitle'" do
+      json = { "subtitle" => "live at the O2" }
+
+      track_decorated = TrackDecorator.new(json)
+
+      expect(track_decorated.subtitle).to eq(json["subtitle"])
+    end
+
+    it "decorates 'duration_formatted'" do
+      json = { "duration" => 202 }
+      duration_formatted = "PT00H03M22S"
+
+      track_decorated = TrackDecorator.new(json)
+
+      expect(track_decorated.duration_formatted).to eq(duration_formatted)
+    end
+
+    it "decorates 'artist_name'" do
+      artist_name = "Tony Crombie feat. Robert Robertson"
+      json        = { "artist" => { "name" => artist_name } }
+
+      track_decorated = TrackDecorator.new(json)
+
+      expect(track_decorated.artist_name).to eq(artist_name)
+    end
+
+    it "decorates 'direct_contributors'" do
+      json = { "contributors" => [
+               { "name"   => "Tony Crombie",
+                 "direct" => true,
+                 "roles"  => ["Artist"] },
+
+               { "name"   => "Robert Robertson",
+                 "direct" => true,
+                 "roles"  => ["FeaturedArtist"] },
+
+               { "name"   => "Bill Wallis",
+                 "direct" => true,
+                 "roles"  => ["Mixer"] }
+             ] }
+
+      direct_contributors = [ { "direct" => true,
+                                "name"   => "Tony Crombie",
+                                "roles"  => ["Artist"] },
+
+                              { "direct" => true,
+                                "name"   => "Robert Robertson",
+                                "roles"  => ["FeaturedArtist"] },
+
+                              { "direct" => true,
+                                "name"   => "Bill Wallis",
+                                "roles"  => ["Mixer"] } ]
+
+      track_decorated = TrackDecorator.new(json)
+
+      expect(track_decorated.direct_contributors).to eq(direct_contributors)
+    end
+
+    it "decorates 'indirect_contributors'" do
+      json = { "contributors" => [
+               { "name"   => "Tony Crombie",
+                 "direct" => false,
+                 "roles"  => ["Artist"] },
+
+               { "name"   => "Robert Robertson",
+                 "direct" => false,
+                 "roles"  => ["FeaturedArtist"] },
+
+               { "name"   => "Bill Wallis",
+                 "direct" => false,
+                 "roles"  => ["Mixer"] }
+             ] }
+
+      indirect_contributors = [ { "direct" => false,
+                                  "name"   => "Tony Crombie",
+                                  "roles"  => ["Artist"] },
+
+                                { "direct" => false,
+                                  "name"   => "Robert Robertson",
+                                  "roles"  => ["FeaturedArtist"] },
+
+                                { "direct" => false,
+                                  "name"   => "Bill Wallis",
+                                  "roles"  => ["Mixer"] } ]
+
+      track_decorated = TrackDecorator.new(json)
+
+      expect(track_decorated.indirect_contributors).
+        to eq(indirect_contributors)
+    end
+
+    it "decorates 'record_label_name'" do
+      record_label_name = "Harrison James Music"
+      json = { "record_labels" => [{ "name" => record_label_name }] }
+
+      track_decorated = TrackDecorator.new(json)
+
+      expect(track_decorated.record_label_name).to eq(record_label_name)
+    end
+
+    it "decorates 'pline'" do
+      pline = "2010 Harrison James Music"
+      json  = { "record_labels" => [{ "p_line" => pline }] }
+
+      track_decorated = TrackDecorator.new(json)
+
+      expect(track_decorated.pline).to eq(pline)
+    end
+
+    it "decorates 'tags'" do
+      json = { "tags" => [
+               { "name" => "Dance",      "classification" => "genre" },
+               { "name" => "Tech House", "classification" => "genre" },
+             ] }
+
+      tags = [ { "classification" => "genre", "name" => "Dance" },
+               { "classification" => "genre", "name" => "Tech House" } ]
+
+
+      track_decorated = TrackDecorator.new(json)
+
+      expect(track_decorated.tags).to eq(tags)
+    end
+
+    it "decorates 'explicit'" do
+      json = { "explicit" => true }
+
+      track_decorated = TrackDecorator.new(json)
+
+      expect(track_decorated.explicit).to eq(json["explicit"])
+    end
+  end
+end
