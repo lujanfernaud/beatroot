801e63c823ae997ab2b35b6453e6d3e4ca70d635
lujanfernaud <lujanfernaud@gmail.com>
2018-10-20 18:44:11 +0100

    Render tracks in home page

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class TracksController < ApplicationController
   def index
+    @tracks = Beatroot.tracks
   end
 end
--- spec/features/tracks_spec.rb
+++ spec/features/tracks_spec.rb
@@ -2,10 +2,27 @@ require 'rails_helper'
 
 RSpec.feature "Tracks", type: :feature do
   context "when visiting root" do
-    it "shows Tracks page" do
+    it "shows Tracks page", :vcr do
       visit root_path
 
       expect(page).to have_content("Tracks")
+    end
+
+    it "shows list of tracks" do
+      url     = "#{Beatroot::URL}/tracks"
+      token   = Beatroot::TOKEN
+      headers = { headers: { "Authorization" => "Token token=#{token}" } }
+      tracks  = { "tracks" => [{
+        "artist" => { "name" => "Rebecca Ferguson" },
+        "full_title" => "Too Good To Lose"}] }
+
+      allow(HTTParty).to receive(:get).with(url, headers).and_return(tracks)
+
+      visit root_path
+
+      within ".track__title" do
+        expect(page).to have_content("Rebecca Ferguson - Too Good To Lose")
+      end
     end
   end
 end
