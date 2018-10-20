54dc6d98a55042890e66283326f5e14cc7c3d353
lujanfernaud <lujanfernaud@gmail.com>
2018-10-20 18:25:40 +0100

    Update VCR config
    Do not allow http connections when a cassette doesn't exist.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,11 +2,9 @@ require 'simplecov'
 SimpleCov.start
 
 require 'vcr'
-
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
-  c.allow_http_connections_when_no_cassette = true
   c.configure_rspec_metadata!
 end
 
