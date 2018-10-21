5683be05a9580f5b4572a1a3666e6ce8ca867899
lujanfernaud <lujanfernaud@gmail.com>
2018-10-21 14:21:54 +0100

    Refactor TrackXmlBuilder#generate_markup
    Extract methods to increase readability.

--- app/services/track_xml_builder.rb
+++ app/services/track_xml_builder.rb
@@ -14,7 +14,6 @@ class TrackXmlBuilder
 
   def run
     xml.instruct!
-
     generate_markup
   end
 
@@ -24,63 +23,94 @@ class TrackXmlBuilder
 
     def generate_markup
       xml.Track do
-        if isrc
-          xml.ISRC isrc
-        end
+        add_isrc
+        add_reference_title
+        add_duration
+        add_artist_name
+        add_contributors
+        add_indirect_contributors
+        add_record_label_name
+        add_pline
+        add_genres
+        add_parental_warning
+      end
+    end
 
-        if reference_title?
-          xml.ReferenceTitle do
-            xml.TitleText title
-            xml.SubTitle  subtitle
+    def add_isrc
+      if isrc
+        xml.ISRC isrc
+      end
+    end
+
+    def add_reference_title
+      if reference_title?
+        xml.ReferenceTitle do
+          xml.TitleText title
+          xml.SubTitle  subtitle
+        end
+      end
+    end
+
+    def add_duration
+      if duration
+        xml.Duration duration
+      end
+    end
+
+    def add_artist_name
+      if artist_name
+        xml.ArtistName artist_name
+      end
+    end
+
+    def add_contributors
+      if direct_contributors
+        direct_contributors.each do |contributor|
+          xml.Contributor do
+            xml.Name contributor.name
+            xml.Role contributor.roles
           end
         end
+      end
+    end
 
-        if duration
-          xml.Duration duration
-        end
-
-        if artist_name
-          xml.ArtistName artist_name
-        end
-
-        if direct_contributors
-          direct_contributors.each do |contributor|
-            xml.Contributor do
-              xml.Name contributor.name
-              xml.Role contributor.roles
-            end
+    def add_indirect_contributors
+      if indirect_contributors
+        indirect_contributors.each do |contributor|
+          xml.IndirectContributor do
+            xml.Name contributor.name
+            xml.Role contributor.roles
           end
         end
+      end
+    end
 
-        if indirect_contributors
-          indirect_contributors.each do |contributor|
-            xml.IndirectContributor do
-              xml.Name contributor.name
-              xml.Role contributor.roles
-            end
-          end
-        end
+    def add_record_label_name
+      if record_label_name
+        xml.RecordLabelName record_label_name
+      end
+    end
 
-        if record_label_name
-          xml.RecordLabelName record_label_name
+    def add_pline
+      if pline
+        xml.PLine do
+          xml.Year      pline.year
+          xml.PLineText pline.text
         end
+      end
+    end
 
-        if pline
-          xml.PLine do
-            xml.Year      pline.year
-            xml.PLineText pline.text
-          end
+    def add_genres
+      if genres
+        genres.each do |genre|
+          xml.Genre genre
         end
+      end
+    end
 
-        if genres
-          genres.each do |genre|
-            xml.Genre genre
-          end
-        end
-
-        if parental_warning
-          xml.ParentalWarningType parental_warning
-        end
+    def add_parental_warning
+      if parental_warning
+        xml.ParentalWarningType parental_warning
       end
     end
 
